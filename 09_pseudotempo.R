1e7f9e6edc887ee682c58666e2fdce29baab3f73
Joyce Karoline <83780948+joyce-karol@users.noreply.github.com>
2026-06-11 20:31:57 -0300

    Enhance pseudotempo.R with package installations and analysis
    Added installation of required packages and modified pseudotime analysis for lncRNAs.

--- 09_pseudotempo.R
+++ 09_pseudotempo.R
@@ -1,10 +1,20 @@
-
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+devtools::install_github('cole-trapnell-lab/monocle3')
+install.packages('Seurat')
+install.packages("remotes")
+install.packages("R.utils")
+devtools::install_github("satijalab/seurat-wrappers")
 library(Seurat)
 library(monocle3)
 library(SeuratWrappers)
 
+setwd("~/denv")
+
+seurat_obj <- readRDS("RDS/sct_harmony_annotation.rds")
 subset_obj <- subset(seurat_obj, subset = group %in% 
-                    c("control", "acute", "defervescence"))
+                       c("control", "acute", "defervescence"))
 
 # Subset por tipos celulares da linhagem B
 b_cells <- subset(subset_obj, subset = celltype_0.5 %in% 
@@ -42,8 +52,33 @@ plot_cells(cds, color_cells_by = "pseudotime", label_cell_groups = FALSE)
 if("LNC_DHF_01" %in% rownames(cds)) {
   plot_genes_in_pseudotime(cds["LNC_DHF_01", ], color_cells_by = "condition")
 }
+plot_genes_in_pseudotime(cds[c("PVT1", "MIR22HG", "FTX"), ],
+                         min_expr = 0.1,
+                         cell_size = 1,
+                         color_cells_by = "dengue_classification")
 
 # Identificar lncRNAs que variam significativamente ao longo da trajetória
-pr_test <- graph_test(cds, neighbor_graph = "principal_graph")
-seus_lncRNAs <- # lista de lncRNA
-sig_lncRNAs <- pr_test[pr_test$gene_short_name %in% seus_lncRNAs & pr_test$q_value < 0.05, ]
+pr_test <- graph_test(cds, neighbor_graph = "principal_graph", cores = 4)
+
+# Filtrar lncRNAs com forte associação ao pseudotempo
+sig_lnc <- subset(pr_test, morans_I > 0.1 &q_value < 0.05 & status == "OK")
+
+
+plot_cells(cds, color_cells_by = "pseudotime", label_cell_groups = FALSE, 
+           label_leaves = FALSE, label_branch_points = FALSE)
+plot_cells(cds, color_cells_by = "celltype_0.5_fine", label_cell_groups = FALSE)
+
+plot_cells(cds, color_cells_by = "dengue_classification", label_cell_groups = FALSE) +
+  facet_wrap(~condition)
+
+
+# Selecionar os lncRNAs que passaram no filtro (morans_I > 0.1, q_value < 0.05)
+sig_lnc_ids <- rownames(pr_test[pr_test$gene_short_name %in% seus_lncRNAs & 
+                                  pr_test$morans_I > 0.1 & 
+                                  pr_test$q_value < 0.05, ])
+# Obter matriz de expressão (normalizada) para esses lncRNAs, ordenar células por pseudotempo
+expr <- exprs(cds)[sig_lnc_ids, order(pseudotime(cds))]
+# Plotar heatmap (usar pheatmap ou ComplexHeatmap)
+pheatmap(expr, scale = "row", show_colnames = FALSE, 
+         annotation_col = data.frame(condition = colData(cds)$condition[order(pseudotime(cds))],
+                                     pseudotime = pseudotime(cds)[order(pseudotime(cds))]))
